262f6a28c770f7bfa702a013f8ebbaa1a0cc6f43
Diego Vega <diegovegabar@hotmail.com>
2019-06-08 20:39:47 -0500

    Authorization and dtos

---
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace DatingApp.API.Dtos
+{
+    public class UserForRegisterDto
+    {
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        [StringLength(8, MinimumLength=4, ErrorMessage="El password debe estar entre 4 y 8 caracteres")]
+        public string password { get; set; }
+    }
+}
